f4efb66f19091ee63d14529e6386498fe79b44bd
max-dankow <max.dankow@gmail.com>
2017-02-13 18:33:09 +0300

    [29] Dankovtsev. Pain of Android. Both parts.

---
+++ Task1/app/src/main/java/ru/mipt/diht/maximdankovtsev/myapplication/MainActivity.java
@@ -0,0 +1,122 @@
+package ru.mipt.diht.maximdankovtsev.myapplication;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    // Bundle keys
+    private static final String STATE_VALUE = "numberValue";
+    private static final String STATE_AUTO_MODE = "autoIncrement";
+    private TextView textViewNum;
+    private int value = 0;
+    private boolean autoMode = false;
+    private AutoIncrementer autoIncrementer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonInc = (Button) findViewById(R.id.button_inc);
+        textViewNum = (TextView) findViewById(R.id.text_num);
+        final MainActivity thisActivity = this;
+
+        buttonInc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!autoMode) {
+                    autoIncrementer = new AutoIncrementer();
+                    autoIncrementer.link(thisActivity);
+                    autoIncrementer.execute();
+                } else {
+                    if (autoIncrementer != null && !autoIncrementer.isCancelled()) {
+                        autoIncrementer.cancel(false);
+                    }
+                }
+                autoMode = !autoMode;
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(STATE_VALUE, value);
+        outState.putBoolean(STATE_AUTO_MODE, autoMode);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        value = savedInstanceState.getInt(STATE_VALUE);
+        textViewNum.setText(String.valueOf(value));
+        // Restore auto increment if it was turned on.
+        autoMode = savedInstanceState.getBoolean(STATE_AUTO_MODE);
+        if (autoMode) {
+            autoIncrementer = (AutoIncrementer) getLastNonConfigurationInstance();
+            if (autoIncrementer == null) {
+                autoMode = false;
+            } else {
+                autoIncrementer.link(this);
+                value = autoIncrementer.getLocalValue();
+            }
+        }
+    }
+
+    @Override
+    public Object onRetainNonConfigurationInstance() {
+        if (autoIncrementer != null) {
+            autoIncrementer.unlink();
+            return autoIncrementer;
+        } else {
+            return null;
+        }
+    }
+
+    static private class AutoIncrementer extends AsyncTask<Void, Integer, Void> {
+        int localValue;
+        private MainActivity mainActivity;
+
+        public int getLocalValue() {
+            return localValue;
+        }
+
+        public void link(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        public void unlink() {
+            this.mainActivity = null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            mainActivity.textViewNum.setText(String.valueOf(values[0]));
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                while(!isCancelled()) {
+                    Thread.sleep(1000);
+                    // While swapping links due to change of configuration,
+                    // we have to save progress in local variable.
+                    localValue += 1;
+                    if (mainActivity != null) {
+                        mainActivity.value += 1;
+                        publishProgress(mainActivity.value);
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    };
+}
