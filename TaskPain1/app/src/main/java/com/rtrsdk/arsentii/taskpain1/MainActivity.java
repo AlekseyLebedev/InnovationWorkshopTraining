59441c284992a3ecf0ab58fe6eca309eb23667fb
arsentii <arsentii.kaptsan@gmail.com>
2017-02-19 13:34:08 +0300

    Init commit with task Pain1

---
+++ TaskPain1/app/src/main/java/com/rtrsdk/arsentii/taskpain1/MainActivity.java
@@ -0,0 +1,45 @@
+package com.rtrsdk.arsentii.taskpain1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final private String COUNT = "count";
+
+    private int count = 0;
+
+    private TextView countTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt(COUNT);
+        }
+
+        countTv = (TextView) findViewById(R.id.count_tv);
+        Button countBtn = (Button) findViewById(R.id.count_btn);
+
+        countTv.setText(Integer.toString(count));
+
+        countBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countTv.setText(Integer.toString(++count));
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(COUNT, count);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+}
