2f1c386091d9a0166ad5bc29750ec2f47d68897b
max-dankow <max.dankow@gmail.com>
2017-03-09 20:07:43 +0300

    [7] Добавил получение буфера с камеры в превью (NV21)

--- CameraExample/app/src/main/java/ru/mipt/diht/maximdankovtsev/cameraexample/CameraActivity.java
+++ CameraExample/app/src/main/java/ru/mipt/diht/maximdankovtsev/cameraexample/CameraActivity.java
@@ -2,9 +2,11 @@ package ru.mipt.diht.maximdankovtsev.cameraexample;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +17,8 @@ import android.view.SurfaceView;
 
 import java.io.IOException;
 
-public class CameraActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+public class CameraActivity extends AppCompatActivity
+        implements SurfaceHolder.Callback, Camera.PreviewCallback {
 
     private static final String TAG = "CameraActivity";
 
@@ -60,11 +63,11 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     }
 
     private boolean tryStartCameraPreview() {
-        Log.d(TAG, "tryStartCameraPreview");
         if (mCamera != null && mIsSurfaceCreated) {
             try {
                 mCamera.setPreviewDisplay(mSurfaceView.getHolder());
                 mCamera.startPreview();
+                setupPreviewCallback(mCamera);
                 return true;
             } catch (IOException e) {
                 e.printStackTrace();
@@ -104,6 +107,20 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
         mIsSurfaceCreated = false;
     }
 
+    // Вызвается на каждый кадр превью, ищет самый яркий пиксель.
+    @Override
+    public void onPreviewFrame(final byte[] data, final Camera camera) {
+        Log.d(TAG, "onPreviewFrame");
+        processFrame(data);
+        // Закончили обрабутку буфера, теперь можно его внось добавить
+        // в очередь для получения следующего кадра.
+        camera.addCallbackBuffer(data);
+    }
+
+    private void processFrame(final byte[] data) {
+
+    }
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -138,19 +155,39 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     }
 
     // Получение и настройка CameraWithId
+    @Nullable
     CameraWithId initializeCamera() {
         CameraWithId cameraWithId = getCameraInstance();
         Camera newCamera = cameraWithId.camera();
+        if (newCamera == null) {
+            return null;
+        }
+        adjustCamera(newCamera);
+        return cameraWithId;
+    }
 
-        // Настройка режима фокусировки и размера изображения
-        assert newCamera != null;
-        Camera.Parameters parameters = newCamera.getParameters();
+    // Настройка буфера для получения кадров в превью
+    private void setupPreviewCallback(final Camera camera) {
+        byte[] frameBuffer = new byte[calculateFrameBufferSize(camera)];
+        camera.addCallbackBuffer(frameBuffer);
+        camera.setPreviewCallbackWithBuffer(this);
+        Log.d(TAG, "setupPreviewCallback: callback buffer is ready");
+    }
+
+    // Настройка режима фокусировки и размера изображения
+    private void adjustCamera(@NonNull Camera camera) {
+        Camera.Parameters parameters = camera.getParameters();
         parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
         parameters.setPreviewSize(PREVIEW_WIDTH, PREVIEW_HEIGHT);
-        newCamera.setParameters(parameters);
-        updateCameraRotation(newCamera);
-        Log.d(TAG, "initializeCamera: parameters are set");
-        return cameraWithId;
+        parameters.setPictureFormat(ImageFormat.NV21);
+        camera.setParameters(parameters);
+        updateCameraRotation(camera);
+    }
+
+    private int calculateFrameBufferSize(final Camera camera) {
+        Camera.Size size = camera.getParameters().getPreviewSize();
+        int bitsPerPixel = ImageFormat.getBitsPerPixel(ImageFormat.NV21);
+        return size.height * size.width * bitsPerPixel / 8;
     }
 
     // Пытается найти и открыть первую доступную камеру.
@@ -166,10 +203,13 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
                 newCameraId = i;
                 Log.d(TAG, "getCameraInstance: Camera is found and opened, id:" + newCameraId);
                 break;
-            } catch (Exception e) {
-                e.printStackTrace();
-                finish();
+            } catch (Exception ignore) {
             }
+        }
+        // Не удалось получить камеру
+        if (newCamera == null) {
+            // можно сообщить о проблеме
+            finish();
         }
         return new CameraWithId(newCamera, newCameraId);
     }
@@ -206,10 +246,15 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     // и, если целевая поверхность была создана раньше камеры, то присоеденить новую камеру к ней.
     private void onCameraAccessGranted() {
         CameraWithId cameraWithId = initializeCamera();
-        mCamera = cameraWithId.camera();
-        mCameraId = cameraWithId.id();
-        if (mIsSurfaceCreated) {
-            tryStartCameraPreview();
+        if (cameraWithId == null) {
+            // можно сообщить о неудаче
+            finish();
+        } else {
+            mCamera = cameraWithId.camera();
+            mCameraId = cameraWithId.id();
+            if (mIsSurfaceCreated) {
+                tryStartCameraPreview();
+            }
         }
     }
 
